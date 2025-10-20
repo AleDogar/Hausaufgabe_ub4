8a63b9b70e49dff5496574176063196bc438ea45
Alesia <dogar_alesia@yahoo.com>
2025-10-21 02:08:19 +0300

    subpunctul 2

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,12 @@ public class Main {
         int[] tastaturen = {40, 35, 70, 15, 45};
         int billigste = shopUtils.findCheapestKeyboard(tastaturen);
         System.out.println("Billigste Tastatur: " + billigste);
+
+        // 2. Teuersten Gegenstand finden
+        int[] tastaturen2 = {15, 20, 10, 35};
+        int[] usbLaufwerke = {20, 15, 40, 15};
+        int teuerster = shopUtils.findMostExpensiveItem(tastaturen2, usbLaufwerke);
+        System.out.println("Teuerster Gegenstand: " + teuerster);
     }
 
     public static void main(String[] args) {
--- src/ShopUtils.java
+++ src/ShopUtils.java
@@ -2,14 +2,31 @@ public class ShopUtils {
 
     // 1. Methode: Finde den billigsten Preis einer Tastatur
     public int findCheapestKeyboard(int[] keyboardPrices) {
-        int minPrice = keyboardPrices[0];  // Setze erstes Element als Minimum
-
+        int minPrice = keyboardPrices[0];
         for (int i = 1; i < keyboardPrices.length; i++) {
             if (keyboardPrices[i] < minPrice) {
-                minPrice = keyboardPrices[i];  // Aktualisiere Minimum, falls kleiner gefunden
+                minPrice = keyboardPrices[i];
+            }
+        }
+        return minPrice;
+    }
+
+    // 2. Methode: Finde den teuersten Gegenstand
+    public int findMostExpensiveItem(int[] keyboardPrices, int[] usbPrices) {
+        int maxPrice = keyboardPrices[0];
+
+        for (int i = 1; i < keyboardPrices.length; i++) {
+            if (keyboardPrices[i] > maxPrice) {
+                maxPrice = keyboardPrices[i];
             }
         }
 
-        return minPrice;  // Rückgabe des kleinsten Preises
+        for (int i = 0; i < usbPrices.length; i++) {
+            if (usbPrices[i] > maxPrice) {
+                maxPrice = usbPrices[i];
+            }
+        }
+
+        return maxPrice;
     }
 }
